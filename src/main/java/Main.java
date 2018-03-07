35a8b58acf632e0eddfa889f726e2f0c993832fd
marcllena <marcllena@hotmail.com>
2018-03-07 16:41:36 +0100

    Afegir log4j

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,8 @@
+import org.apache.log4j.Logger;
 /**
  * Created by jordi on 07/03/2018.
  */
 public class Main {
-
+    final static Logger logger = Logger.getLogger(Main.class);
+    logger.de
 }
