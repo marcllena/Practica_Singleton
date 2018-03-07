24e73a2e3ca1e08184a38322c9f7275cc9735f74
GABRI <gabrigoca@gmail.com>
2018-03-07 16:32:09 +0100

    constructor al singleton

--- src/main/java/I18NManager.java
+++ src/main/java/I18NManager.java
@@ -1,10 +1,19 @@
+import java.util.HashMap;
+import java.util.ResourceBundle;
+
 public class I18NManager {
-    private static I18NManager ourInstance = new I18NManager();
+    private static I18NManager ourInstance;
+
+    private HashMap<String,ResourceBundle> data;
 
     public static I18NManager getInstance() {
+        if (ourInstance==null)
+            ourInstance = new I18NManager();
         return ourInstance;
     }
 
     private I18NManager() {
+        data= new HashMap<String, ResourceBundle>();
+
     }
 }
